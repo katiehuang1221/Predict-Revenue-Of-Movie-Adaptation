f0be4c4e2de5503fd6ead3c6bafaab029d7abd5a
katie <kofanhuang@gmail.com>
2021-01-14 23:54:41 -0800

    Added selenium_practice. Added fictiondb.py for selenium functions

---
+++ py/fictiondb.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+import time, os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+chromedriver = "/Applications/chromedriver" # path to the chromedriver executable
+os.environ["webdriver.chrome.driver"] = chromedriver
+
+# Open automated Chrome with fictionDB webpage
+driver = webdriver.Chrome(chromedriver)
+driver.get("https://www.fictiondb.com/search/search.htm")
+
+def search(author):
+    driver.get("https://www.fictiondb.com/search/search.htm")
+    search_bar = driver.find_element_by_xpath("//input[@name='author'][@type='text']")
+    search_bar.clear()
+    search_bar.send_keys(author)
+    search_bar.send_keys(Keys.RETURN)
+    
+    # Click on the author link
+    try:
+
+        author_r = ", ".join(author.split()[::-1])
+        elem = driver.find_element_by_link_text(author_r)
+        elem.click()
+    except:
+        pass
+
+    publication = driver.find_elements_by_xpath("//a[@itemprop='datePublished']")
+    dates = [s.text.split('-') for s in publication]
+    years = [int(y[-1]) for y in dates]
+
+    return years
+
+
+def find_years():
+    # Find publications and create list of year
+    publication = driver.find_elements_by_xpath("//a[@itemprop='datePublished']")
+    dates = [s.text.split('-') for s in publication]
+    years = [int(y[-1]) for y in dates]
+
+    return years
--- py/loadpage.py
+++ py/loadpage.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from IPython.core.display import display, HTML
 
-def loadpage(file_html):
-    with open('../'+ file_html +') as page:
-        test_html = page.read()
-    soup = BeautifulSoup(test_html,'lxml')
+def get_soup(url):
+    response = requests.get(url)
+    page = response.text
+    soup = BeautifulSoup(page, "lxml")
     return soup
