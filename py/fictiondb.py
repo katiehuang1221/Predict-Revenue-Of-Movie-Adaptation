bca64a1f58eda2add0cedba9db2f91d1906db77a
Katie Huang <kofanhuang@gmail.com>
2021-01-17 12:12:38 -0800

    Refined search(author) function in fictiondb.py. Added pickled author_list_set & author_book_list (scraped so far)

--- py/fictiondb.py
+++ py/fictiondb.py
@@ -53,3 +53,62 @@ def find_years():
     years = [int(y[-1]) for y in dates]
 
     return years
+
+
+# Added sleep time in between actions
+def search(author):
+    author_dict={}
+
+    driver.get("https://www.fictiondb.com/search/search.htm")
+    search_bar = driver.find_element_by_xpath("//input[@name='author'][@type='text']")
+    search_bar.clear()
+    search_bar.send_keys(author)
+    search_bar.send_keys(Keys.RETURN)
+    
+    # Rest for 3 seconds
+    time.sleep(3)
+    # Press enter in case there's pop up ad
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.RETURN)
+    actions.perform()
+    
+    # Search for publications first
+    # If error (multiple authors page), pass
+    try:
+        time.sleep(1)
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        time.sleep(3)
+        publication = driver.find_elements_by_xpath("//a[@itemprop='datePublished']")
+        dates = [(s.text+'0').split('-') for s in publication]
+        years = [int(y[-1])//10 for y in dates if int(y[-1])//10 != 0]
+    except:
+        print("multiple search results of author")
+        pass    
+    
+    # Click on the author link
+    try:
+        author_r = ", ".join(author.split()[::-1])
+        elem = driver.find_element_by_link_text(author_r)
+        elem.click()
+    except:
+        print("can't find author")
+        pass
+
+    try:
+        time.sleep(1)
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.RETURN)
+        actions.perform()
+        time.sleep(2)
+        publication = driver.find_elements_by_xpath("//a[@itemprop='datePublished']")
+        dates = [(s.text+'0').split('-') for s in publication]
+        years = [int(y[-1])//10 for y in dates if int(y[-1])//10 != 0]
+    except:
+        print("can't find publication")
+        pass
+    
+    author_dict[author] = years
+
+    return author_dict
